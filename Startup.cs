f18daf582cd81bd7244db8828467e5b812365b24
Sage-tech <62355285+Sage-tech@users.noreply.github.com>
2021-03-04 09:06:15 -0700

    commit

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,6 +13,13 @@ namespace Explore_California_Web
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,10 +29,20 @@ namespace Explore_California_Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+
+            if (configuration.GetValue<bool>("EnableDeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async(context, next) =>
+            { 
+           if (context.Request.Path.Value.Contains("invalid"))
+                throw new Exception("ERROR!");
+
+            await next();
+        });
 
             app.UseFileServer();
         }
