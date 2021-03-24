bd039b9b75250db1e1c041173aca94bb722c961f
Sage-tech <62355285+Sage-tech@users.noreply.github.com>
2021-03-24 10:52:08 -0700

    create view

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -9,23 +9,30 @@ namespace Explore_California_Web.Controllers
     [Route("blog")]
     public class BlogController : Controller
     {
-        [Route("")] 
+        [Route("")]
         public IActionResult Index()
-        { 
+        {
             return View();
         }
         [Route("{year:min(2000)}/{month:int}/{key}")]
         public IActionResult Post(int year, int month, string key)
-       
-            {
+
+        {
 
             ViewBag.Title = "Myblog post";
             ViewBag.Posted = DateTime.Now;
             ViewBag.Author = "Sierra Soto";
             ViewBag.Body = "This is a great blog post, don't you think?";
-    
+
 
             return View();
         }
+
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
     }
 }
